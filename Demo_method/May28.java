52d031294b9b7a7d7b86d36939f64d9dd056770d
akshara2006-psit <gakshara21@gmail.com>
2026-05-28 15:30:30 +0530

    28 may done

---
+++ Demo_method/May28.java
@@ -0,0 +1,14 @@
+package Demo_method;
+
+public class May28 {
+    static void printWelcomeMessage(){
+        System.out.println("Hi!!!!!! How r you");
+
+    }
+    public static void main(String[] args) {
+        printWelcomeMessage();
+    }
+}
+
+
+
