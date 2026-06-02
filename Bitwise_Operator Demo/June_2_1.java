06df8b7d5c7c6df076268f84f28bd1bf1fe37d57
akshara2006-psit <gakshara21@gmail.com>
2026-06-02 19:48:57 +0530

    first part of today

---
+++ Bitwise_Operator Demo/June_2_1.java
@@ -0,0 +1,12 @@
+public class June_2_1{
+    public static void main(String[] args) {
+        int a=5;
+        int b=6;
+        System.out.println(a & b);
+        System.out.println(a| b);
+        System.out.println(a^b);
+        System.out.println(~a);
+        System.out.println(a<<1);
+        System.out.println(a>>1);
+    }
+}
