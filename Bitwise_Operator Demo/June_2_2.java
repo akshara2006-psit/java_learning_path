bbb61eb9824e348c26f7481e6c008b2c3ff2a8c5
akshara2006-psit <gakshara21@gmail.com>
2026-06-02 20:31:11 +0530

    second part

---
+++ Bitwise_Operator Demo/June_2_2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class June_2_2 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        if((num & 1)==1){
+            System.out.println(num+" is odd number");
+        }
+        else
+            System.out.println(num+" is even number");
+    }
+}
+//Check numbera are even or odd using bitwise operator
