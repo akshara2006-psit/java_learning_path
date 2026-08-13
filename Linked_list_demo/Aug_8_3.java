8e591c28b988631c50d5279a4e0a26096337cf68
akshara2006-psit <gakshara21@gmail.com>
2026-08-14 00:11:11 +0530

    first part

---
+++ Linked_list_demo/Aug_8_3.java
@@ -0,0 +1,21 @@
+public class Aug_8_3 {
+    Node removeDuplicates(Node head) {
+        // code here
+        
+        if (head == null)
+            return null;
+
+        Node curr = head;
+        while (curr != null && curr.next != null) {
+
+            if (curr.data == curr.next.data) {
+                curr.next = curr.next.next;
+            } else {
+                curr = curr.next;
+            }
+        }
+
+        return head;
+    }
+}
+// Remove Duplicates from Linked list
