bc1c57b22c7eb06d2a1877923ba234ebfa215169
akshara2006-psit <gakshara21@gmail.com>
2026-08-26 00:06:47 +0530

    first part

---
+++ Recursion_Demo/Aug_18_.java
@@ -0,0 +1,12 @@
+public class Aug_18_{
+   static void print1toN(int n){
+        if(n==0)
+            return;
+        print1toN(n-1);
+        System.out.println(n);
+    }
+    public static void main(String[] args) {
+        print1toN(60);
+    }
+}
+// 1toN
